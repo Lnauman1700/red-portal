539c068e4e1a9acc9bede226a4b428f5a951c4f7
Cole Reinhart <colecooler16@hotmail.com>
2019-04-15 15:23:11 -0400

    - Got code ready for merging master into branch

--- portal/courses.py
+++ portal/courses.py
@@ -5,8 +5,10 @@ bp = Blueprint('courses', __name__)
 @bp.route('/courses', methods=('GET', 'POST'))
 def courses():
     if request.method == 'POST':
+        course_number = request.form['course_number']
         course = request.form['course']
         info = request.form['info']
         print(course)
+        print(course_number)
         print(info)
     return render_template('courses.html')
