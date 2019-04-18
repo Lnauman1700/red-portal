bced556802944b783e771e03be7446eda829a951
Nicolas Machado <nickmachado.99@gmail.com>
2019-04-18 15:16:17 -0400

    I added some dynamic python code to be able to use dabase information once they are set up. - On top of that I added more styling and a logout button for users to be able to click on.

--- portal/my_courses.py
+++ portal/my_courses.py
@@ -1,9 +1,21 @@
 from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for
+from portal.auth import login_required
 
 # Definining the Blueprint and what it's being named
 bp = Blueprint('my_courses',__name__)
 
 # adding the app routing to the 'my_courses'page
 @bp.route('/my_courses')
+@login_required
 def my_courses():
-    return render_template('my_courses.html')
+    table_headers = ["Course", "Location", "Instructor", "Time"]
+
+    table = [
+        ("CSET 155", "Orange Campus", "Zach Fedor", "MTWRF 12:00pm"),
+        ("CSET 160", "Orange Campus", "Zach Fedor", "MTWRF 12:00pm"),
+        ("CSET 170", "Orange Campus", "Zach Fedor", "MTWRF 12:00pm"),
+        ("CSET 180", "Orange Campus", "Zach Fedor", "MTWRF 12:00pm"),
+        ("English Composition", "Mellor", "Patricia Melley", "MWF 9:00am")
+    ]
+
+    return render_template('my_courses.html', table=table, table_headers=table_headers)
