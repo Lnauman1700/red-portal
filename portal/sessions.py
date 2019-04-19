f4ca0257f019001f861630a87a1f99152cb00d33
Lucas Nauman <Lnauman1700@gmail.com>
2019-04-19 13:41:07 -0400

    Beautified the code related to sessions  - removed unneccessary comments  - changed name of some variables to be more consistent  - edited layout of sessions_add.html slightly

--- portal/sessions.py
+++ portal/sessions.py
@@ -10,23 +10,21 @@ bp = Blueprint('sessions', __name__)
 def sessions_index():
     if request.method == 'GET':
         if g.user[3] != 'teacher':
-            error = 'You are not permitted to view this page'
-            return make_response(render_template('error_page.html', error=error), 401)
+            message = 'You are not permitted to view this page'
+            return make_response(render_template('error_page.html', message=message), 401)
         else:
-            # once courses is established, check that g.user is teacher
-            # access sessions (based on teacher in g.user), put them into a variable
             conn = db.get_db()
             cur = conn.cursor()
             cur.execute("SELECT * FROM sessions JOIN courses ON courses.course_id = sessions.course_id WHERE courses.teacher_id = %s;", (g.user[0],))
             sessions = cur.fetchall()
-            # render template, send in previously mentioned value
+
             return render_template('sessions_list.html', sessions=sessions)
 
 @bp.route('/sessions/<int:id>', methods=['GET', 'POST'])
 @login_required
 def sessions_add(id):
 
-    error = None
+    message = None
 
     conn = db.get_db()
     cur = conn.cursor()
@@ -39,42 +37,37 @@ def sessions_add(id):
     session = cur.fetchone()
 
     if request.method == 'GET':
-        # display session info
         if g.user[3] != 'teacher':
-            error = 'You are not permitted to view this page'
-            return render_template('error_page.html', error=error)
+            message = 'You are not permitted to view this page'
+            return render_template('error_page.html', message=message)
         else:
             # if session doesn't exist, then throw error
             if session is None:
-                error = "Session does not exist"
-                return make_response(render_template("error_page.html", error=error), 404)
+                message = "Session does not exist"
+                return make_response(render_template("error_page.html", message=message), 404)
             else:
-                # query up the students who aren't in this session, put it in a value
-                # render the template, send previous value in
                 return render_template('session_add.html', session=session, students=students)
-                    # in the template, the options in the drop-down menu will have value=user id and display email
+
     elif request.method == 'POST':
-        # grab the value from the post
         student_id = int(request.form['student'])
 
         conn = db.get_db()
         cur = conn.cursor()
+        # lists students who are already present in the current session, and the session ID
         cur.execute("SELECT * FROM users_sessions WHERE student = %s AND session = %s", (student_id, id,))
         already_present = cur.fetchone()
 
+        # check to see if that student is already present in the session
         if already_present is not None:
-            error = "Student is already in this session"
-            return render_template('session_add.html', error=error, session=session, students=students)
+            message = "Student is already in this session"
+            return render_template('session_add.html', message=message, session=session, students=students)
         else:
             # query inserting the user and the session into users_sessions
             cur.execute("INSERT INTO users_sessions VALUES (%s, %s)", (student_id, id,))
             conn.commit()
-            error = 'Student successfully added'
-        # set message that says whether a success or a fail
+            message = 'Student successfully added'
 
-            # maybe a query that searches for the student/session value in table
-        # render same template as in GET request, send in error message
-        return render_template('session_add.html', session=session, students=students, id=id, error=error)
+        return render_template('session_add.html', session=session, students=students, id=id, message=message)
 
 @bp.route('/sessions/create', methods=['GET', 'POST'])
 @login_required
@@ -82,13 +75,14 @@ def create_session():
 
     conn = db.get_db()
     cur = conn.cursor()
+    # queries up all of the courses associated with the currently logged in teacher
     cur.execute("SELECT * FROM courses WHERE teacher_id = %s", (g.user[0],))
     courses = cur.fetchall()
 
     if request.method == 'GET':
         if g.user[3] != 'teacher':
-            error = 'You are not permitted to view this page'
-            return render_template('error_page.html', error=error)
+            message = 'You are not permitted to view this page'
+            return render_template('error_page.html', message=message)
         else:
             return render_template('session_create.html', courses=courses)
 
@@ -98,12 +92,13 @@ def create_session():
         course_id = request.form['course_id']
 
         if session_letter is "" or session_time is "" or course_id is "":
-            error = "Please complete entire form"
-            return render_template('session_create.html', error=error, courses=courses)
+            message = "Please complete entire form"
+            return render_template('session_create.html', error=error, message=message)
         else:
             conn = db.get_db()
             cur = conn.cursor()
+            # create a new session using the form data
             cur.execute("INSERT INTO sessions (letter, session_time, course_id) VALUES (%s, %s, %s)", (session_letter, session_time, course_id))
             conn.commit()
-            success = "Session successfuly created"
-            return render_template('session_create.html', success=success, courses=courses)
+            message = "Session successfuly created"
+            return render_template('session_create.html', message=message, courses=courses)
