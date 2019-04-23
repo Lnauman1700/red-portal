1f3927c86da803c14559fcee9299b5b51feafe39
Nicolas Machado <nickmachado.99@gmail.com>
2019-04-23 15:55:05 -0400

    I  got rid of unused imports in the "my_courses.py" file.

--- portal/my_courses.py
+++ portal/my_courses.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, flash, g, redirect, render_template, request, session, url_for, make_response
+from flask import Blueprint, g, render_template, make_response
 from portal.auth import login_required
 from . import db
 
